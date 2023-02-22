4305f778195f21f1a3d2ac7da275f5ac4ba0a9c2
binbin665 <1076153622@qq.com>
2023-02-22 16:10:42 +0800

    feat: 初始化仓库

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,70 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, ConfigEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from 'path'
+//@ts-ignore
+import viteCompression from 'vite-plugin-compression'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+
+export default defineConfig(({ command, mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    base: './', //打包路径
+    plugins: [
+      vue(),
+      // gzip压缩 生产环境生成 .gz 文件
+      viteCompression({
+        verbose: true,
+        disable: false,
+        threshold: 10240,
+        algorithm: 'gzip',
+        ext: '.gz',
+      }),
+    ],
+    // 配置别名
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/style/main.scss";',
+        },
+      },
+    },
+    //启动服务配置
+    server: {
+      // 是否开启 https
+      https: false,
+      // 端口号
+      port: env.VITE_PORT,
+      // 监听所有地址
+      host: '0.0.0.0',
+      // 服务启动时是否自动打开浏览器
+      open: true,
+      // 允许跨域
+      cors: true,
+      // 自定义代理规则
+      proxy: {},
+    },
+    // 生产环境打包配置
+    //去除 console debugger
+    build: {
+      // 设置最终构建的浏览器兼容目标
+      target: 'es2015',
+      // 构建后是否生成 source map 文件
+      sourcemap: false,
+      //  chunk 大小警告的限制（以 kbs 为单位）
+      chunkSizeWarningLimit: 2000,
+      // 启用/禁用 gzip 压缩大小报告
+      reportCompressedSize: false,
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        },
+      },
+    },
+  }
 })
